9f5a20395adda588309f9d777e383d4238c27fd7
philocypher <noreply@philocypher>
2024-10-12 17:31:20 +0800

    Get Domain & IP info function is added

--- server.py
+++ server.py
@@ -5,5 +5,7 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(address)
 server.listen(5)
 
-while True:
-    #conn, client_address = server.accept()
+# while True:
+#     conn, client_address = server.accept()
+#     with conn:
+#         conn
