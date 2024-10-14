6ef0032668ce5f17ac259d8a3bc910bbaf0f0f82
philocypher <noreply@philocypher>
2024-10-14 21:25:55 +0800

    whois client implemented to gather information on domains reliably.

--- info.py
+++ info.py
@@ -1,13 +1,28 @@
+#! /usr/bin/env python3
+
+import sys
 import socket
 import requests
 import intro
 import signal 
+import whois
+
 
 def signal_handler(sig, frame):
     print('\nExiting...')
-    exit(0)
+    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+def domain_info(dom):
+    w = whois.whois(dom)
+    try:
+        for name, val in w.items():
+            if type(val) == list:
+                 for item in val:
+                     print(f"{name}---> {len(val) - len(val) + 1}{item}")
+            else:
+             print(f"{name} --> {val}")
+    except Exception as e:
+        print(e)
 
 
 def get_ip_info(ip_or_domain):
@@ -29,17 +44,24 @@ def get_ip_info(ip_or_domain):
         print(f"Error: Unable to resolve {ip_or_domain}. Please check the domain name.")
         return None
 
+def dns_info(ip):
+    pass
+
+
+
 if __name__ == "__main__":
     intro.spinner_animation(3)
+    signal.signal(signal.SIGINT, signal_handler)
     while True:
         leave = ''
         user_input = input("Enter an IP address or domain name: ")
-        info = get_ip_info(user_input)
-        if info:
-            print("IP Information:")
-            for key, value in info.items():
-                print(f"{key}: {value}")
+        # info = get_ip_info(user_input)
+        domain_info(user_input)
+       # if info:
+       #     print("IP Information:")
+       #     for key, value in info.items():
+       #         print(f"{key}: {value}")
         leave = input("Do u want to contiue? [enter for yes] ")
         if leave != '':
             print("Bye!")
-            break
+            sys.exit(0)
